9c67f27c7501e7cf3dedaca5622f47c4cd539f36
Andrii KOMIAKOV <andrii.komiakov@icloud.com>
2025-09-03 18:39:32 +0300

    added colors

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
-    <div id="all-page-ims">
+    <div id="all-page-ims" data-theme="light">
+        <header class="container-flex">
+            <section>
 
+            </section>
+            <section>
+
+            </section>
+        </header>
     </div>
 </template>
+
+<style lang="scss">
+#all-page-ims {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    white-space: nowrap;
+}
+</style>
