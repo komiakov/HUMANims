7ab77e7bdc4b4cd80ca5d983932ef4b99eab3917
Andrii KOMIAKOV <andrii.komiakov@icloud.com>
2025-09-03 21:33:30 +0300

    change adaptive styles for nav buttons in header

--- frontend/app/components/blocks/_header.vue
+++ frontend/app/components/blocks/_header.vue
@@ -11,9 +11,9 @@ const isActive = (path: string) => {
 <template>
     <header id="header-ims" class="container-flex">
         <section>
-            <elements-button label="Dashboard" class="mbl" icon="home" to="/" :class="isActive('/') ? 'accent' : 'primary'" />
-            <elements-button label="Databases" class="mbl" icon="database" to="/databases" :class="isActive('/databases') ? 'accent' : 'primary'" />
-            <elements-button label="Reports" class="primary mbl" icon="report" :disabled="true" />
+            <elements-button label="Dashboard" class="mbl header-ims__nav-btn" icon="home" to="/" :class="isActive('/') ? 'accent' : 'primary'" />
+            <elements-button label="Databases" class="mbl header-ims__nav-btn" icon="database" to="/databases" :class="isActive('/databases') ? 'accent' : 'primary'" />
+            <elements-button label="Reports" class="primary mbl header-ims__nav-btn" icon="report" :disabled="true" />
         </section>
         <section>
             <elements-button class="primary" icon="notification" :disabled="true" />
@@ -25,5 +25,11 @@ const isActive = (path: string) => {
 #header-ims {
     padding: 12px 24px;
     background-color: var(--bg-secondary);
+}
+
+@media (min-width: 769px) {
+    #header-ims .header-ims__nav-btn{
+        min-width: 134px;
+    }
 }
 </style>
--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,16 +1,23 @@
 <template>
     <div id="all-page-ims" data-theme="dark">
-       <BlocksHeader />
-       <NuxtPage />
+        <BlocksHeader />
+        <NuxtPage />
     </div>
 </template>
 
 <style lang="scss">
 #all-page-ims {
+    display: grid;
+    grid-template-rows: auto 1fr;
+    gap: 12px;
     width: 100vw;
     height: 100vh;
     overflow: hidden;
     white-space: nowrap;
     background-color: var(--bg-main);
+
+    #content-ims {
+        padding: 0 24px 12px;
+    }
 }
 </style>
